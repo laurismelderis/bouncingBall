cd9d6d6b4905883bc9715bb00d2409bf82a2dad4
Lauris <lauris.melderis77@gmail.com>
2020-01-17 18:50:16 +0200

    Spawn ball with click

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Ball from './ball.js';
 
 const canvas = document.getElementById('ball-platform');
 const createBtn = document.getElementById('createAnimation');
+const clrCanvas = document.getElementById('clearCanvas');
 const W = canvas.width;
 const H = canvas.height;
 const ctx = canvas.getContext('2d');
@@ -13,7 +14,7 @@ createBtn.addEventListener('mousedown', () => {
     let bRadius = Number(document.getElementById('bRadius').value);
 	let count = Number(document.getElementById('count').value);
 	if (bRadius == "" || bRadius <= 0){
-		bRadius = 5;
+		bRadius = 15;
 	}
 	if (count == "" || count < 0){
 		count = 5;
@@ -26,6 +27,39 @@ createBtn.addEventListener('mousedown', () => {
 							W, H, bRadius,));
 	}
 });
+
+canvas.addEventListener('mousedown', (evt) => {
+	let xpos = evt.offsetX;
+	let ypos = evt.offsetY;
+	let bRadius = Number(document.getElementById('bRadius').value);
+	let count = Number(document.getElementById('count').value);
+	if (bRadius == "" || bRadius < 0){
+		bRadius = 15;
+	}
+	if (count == "" || count < 0){
+		count = 1;
+	}
+	if (xpos < bRadius) {
+		xpos += bRadius;
+	} else if(xpos > W - bRadius){
+		xpos -= bRadius;
+	}
+	if(ypos < bRadius){
+		ypos += bRadius;
+	} else if(ypos > H - bRadius){
+		ypos -= bRadius;
+	}
+	for (let i = 0; i < count; i++){
+		ballz.push(new Ball(xpos,
+							ypos,
+							randomNumber(-5, 5),
+							randomNumber(-5, 5),
+							W, H, bRadius,));
+	}
+});
+clrCanvas.onclick = () => {
+	ballz = [];
+}
 
 function animation(){
 	requestAnimationFrame(animation);
